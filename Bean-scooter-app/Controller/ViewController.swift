cb8fd59af13354155913962dff25583eedf4bac8
moremirim <moremirim@gmail.com>
2024-04-22 15:19:02 +0900

    Upload draft

---
+++ Bean-scooter-app/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bean-scooter-app
+//
+//  Created by 박미림 on 4/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
