1f31fd71068e53876e30b058dbe4c519e65231a0
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-04-22 15:40:42 +0900

    AccountInfo추가

---
+++ Bean-scooter-app/Model/AccountInfo.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  Bean-scooter-app
+//
+//  Created by t2023-m0114 on 4/22/24.
+//
+
+import Foundation
+
+struct accoiuntInfo {
+    var iD: String
+    var password: String
+    var userName: String
+}
