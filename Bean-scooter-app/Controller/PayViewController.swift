c8e21419d501a75c508edc7376bbff766bc74321
moremirim <moremirim@gmail.com>
2024-04-25 23:29:07 +0900

    editing PayVC regarding UIelements

---
+++ Bean-scooter-app/Controller/PayViewController.swift
@@ -0,0 +1,63 @@
+//
+//  PayViewController.swift
+//  Bean-scooter-app
+//
+//  Created by 박미림 on 4/25/24.
+//
+
+import UIKit
+
+class PayViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UIView {
+    @IBInspectable var borderColor: UIColor {
+        get {
+            let color = self.layer.borderColor ?? UIColor.clear.cgColor
+            return UIColor(cgColor: color)
+        }
+        
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+}
