fb06f6fd95d057d44a8911f0ccd7896f2a58e95f
Haroldfromk <dongik369@naver.com>
2024-04-26 04:10:00 +0900

    record 껍데기만 구현

---
+++ Bean-scooter-app/Controller/RecordTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RecordTableViewController.swift
+//  Bean-scooter-app
+//
+//  Created by Dongik Song on 4/26/24.
+//
+
+import UIKit
+
+class RecordTableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+       
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 100
+        tableView.register(UINib(nibName: "RecordTableViewCell", bundle: nil), forCellReuseIdentifier: "recordCell")
+        
+    }
+    
+
+
+}
+
+extension RecordTableViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        RecordSingleton.shared.array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "recordCell", for: indexPath) as? RecordTableViewCell else {
+            return UITableViewCell() }
+        
+        let time =  RecordSingleton.shared.array[indexPath.row].time
+        let distance = RecordSingleton.shared.array[indexPath.row].distance
+        let price = Int(distance * 10)
+        
+        cell.dateLabel.text = "날짜: \(time)m"
+        cell.distanceLabel.text = "주행거리: \(distance)"
+        cell.priceLabel.text = "금액: \(price)원"
+        
+        return cell
+    }
+    
+    
+    
+}
