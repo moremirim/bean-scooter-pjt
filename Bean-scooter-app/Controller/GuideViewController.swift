9a860ba1acbd278f6a014e66b0ea0def6bd58efb
Nayeon Chae <cny1230@naver.com>
2024-04-26 18:51:52 +0900

    Guide updated: image and text added

--- Bean-scooter-app/Controller/GuideViewController.swift
+++ Bean-scooter-app/Controller/GuideViewController.swift
@@ -7,24 +7,53 @@
 
 import UIKit
 
-class GuideViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    self.navigationController?.isNavigationBarHidden = false
-//
-        // Do any additional setup after loading the view.
+class GuideViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // 이용가이드 더미 데이터
+    let guideData: [GuideModel] = [
+        GuideModel(imageName: "image_1", title: "킥스탠드를 접어주세요"),
+        GuideModel(imageName: "image_2", title: "앞으로 밀어주세요"),
+        GuideModel(imageName: "image_3", title: "시동을 거세요")
+    ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return guideData.count
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = GuideTable.dequeueReusableCell(withIdentifier: "guideList", for: indexPath) as? GuideTableViewCell else {
+            return UITableViewCell ()
+        }
+        
+        
+        let content = guideData[indexPath.row]
+        
+        cell.GuideImage.image = UIImage(named: content.imageName)
+        cell.GuideText.text = content.title
+        return cell
     }
-    */
-
+    
+    
+    @IBOutlet weak var GuideTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GuideTable.register(UINib(nibName: "GuideTableViewCell", bundle: nil), forCellReuseIdentifier: "guideList")
+        
+        
+        GuideTable.dataSource = self
+        GuideTable.delegate = self
+        GuideTable.separatorStyle = .singleLine
+        GuideTable.rowHeight = 100
+        
+        self.navigationController?.isNavigationBarHidden = false
+        
+    }
+    
 }
+
+
+
---
+++ Bean-scooter-app/Model/GuideModel.swift
@@ -0,0 +1,15 @@
+//
+//  GuideModel.swift
+//  Bean-scooter-app
+//
+//  Created by 채나연 on 4/26/24.
+//
+
+import Foundation
+
+struct GuideModel {
+ 
+    var imageName: String
+    var title: String
+    
+}
