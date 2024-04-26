c9428ee29c06ae96a249a40e2f476249985705fc
Haroldfromk <dongik369@naver.com>
2024-04-26 10:38:07 +0900

    Storyboard modified

--- Bean-scooter-app/Controller/SignInViewController.swift
+++ Bean-scooter-app/Controller/SignInViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 
 
 class SignInViewController: UIViewController {
-    
+
     
     //텍스트필드
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
-    
+
     func setupidTF() {
         idTextField.placeholder = " ID를 입력하세요"
         
@@ -64,9 +64,8 @@ class SignInViewController: UIViewController {
         
         //입력된 id가 회원가입되어있을 경우, 텍스트필드 내용을 마이페이지로 보내고 다음화면으로 이동
         if let account = AccountModel.accountModel.accountInfoArr.first(where: { $0.iD == idText && $0.passWord == pwText }) {
-            
-            if let tabVC = self.storyboard?.instantiateViewController(withIdentifier: "tapVC") as? TabbarViewController {
 
+            if let tabVC = self.storyboard?.instantiateViewController(withIdentifier: "tapVC") as? TabbarViewController {
                 
                 // ProfileViewController로 데이터 전달
                 for viewController in tabVC.viewControllers ?? [] {
@@ -76,15 +75,12 @@ class SignInViewController: UIViewController {
                         break // 데이터를 전달한 후 반복문 종료
                     }
                 }
-                
 
-                tabVC.modalTransitionStyle = .crossDissolve
-                tabVC.modalPresentationStyle = .fullScreen
-                self.present(tabVC, animated: true)
+                self.navigationController?.pushViewController(tabVC, animated: true)
             } else {
                 print("NextViewController를 인스턴스화할 수 없습니다.")
             }
-        }
+                    }
         
         //입력된 id 정보가 가입 안되어있을 경우 안내 알럿
         else {
@@ -102,7 +98,7 @@ class SignInViewController: UIViewController {
         createAccountBT.setTitle("create account", for: .normal)
         createAccountBT.setTitleColor(mainColor, for: .normal)
     }
-    
+
     
     //알럿
     func showAlert(message: String) {
@@ -117,9 +113,7 @@ class SignInViewController: UIViewController {
     //회원가입페이지로 이동
     @IBAction func createAccountButton(_ sender: Any) {
         guard let SignupVC = self.storyboard?.instantiateViewController(identifier: "SignupVC") else {return}
-        SignupVC.modalTransitionStyle = .crossDissolve
-        SignupVC.modalPresentationStyle = .fullScreen
-        self.present(SignupVC, animated: true)
+        self.navigationController?.pushViewController(SignupVC, animated: true)
     }
     
     // 저장된 계정 정보를 로드하는 메서드
@@ -127,7 +121,7 @@ class SignInViewController: UIViewController {
         // AccountModel 인스턴스를 사용하여 계정 정보를 가져옴
         let accountInfoArr = AccountModel.accountModel.accountInfoArr
     }
-    
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -137,6 +131,6 @@ class SignInViewController: UIViewController {
         // 저장된 계정 정보 로드
         loadAccounts()
     }
-    
-    
+
+
 }
