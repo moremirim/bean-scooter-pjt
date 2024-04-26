dd201ed2094f4dbc83ba4c6268fc66f96b4f2a4d
jungkweon93 <jungkweon93@gmail.com>
2024-04-26 18:14:45 +0900

    시작 애니메이션 수정 및 쿠폰 페이지

---
+++ Bean-scooter-app/Model/CouponModel.swift
@@ -0,0 +1,14 @@
+//
+//  CouponModel.swift
+//  Bean-scooter-app
+//
+//  Created by 박중권 on 4/26/24.
+//
+
+import Foundation
+
+struct CouponModel {
+    let imageName: String
+    let discountRate: String
+    let dateLabel: String
+}
